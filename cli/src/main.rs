96c09c3dae9dd8b2ac7d7b1094e9549b072311d5
harontaiko <harontaiko@gmail.com>
2023-11-26 18:35:49 +0300

    intitial commit

---
+++ cli/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let mut mars_weight = calculate_weight_on_mars(100.0);
+    mars_weight = mars_weight * 1000.0;
+    println!("Weight on Mars: {} grams", mars_weight);
+}
+
+
+fn calculate_weight_on_mars(weight: f32) -> f32 {
+    (weight / 9.81) *3.711
+}
---
+++ cli/target/debug/cli.d
@@ -0,0 +1,1 @@
+C:\Users\uncle\Desktop\Rust\cli\target\debug\cli.exe: C:\Users\uncle\Desktop\Rust\cli\src\main.rs
