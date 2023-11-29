80b947cf2269a047b878f30850ea7b25ec7c0d9b
harontaiko <harontaiko@gmail.com>
2023-11-29 15:38:19 +0300

    completed

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,30 +1,22 @@
 use std::io;
 
 fn main() {
+    println!("What is Your Weight in (Kgs): ");
+
     let mut input = String::new();
 
-    io::stdin().read_line(&mut input);
+    io::stdin().read_line(&mut input).unwrap();
 
-    borrow_string(&input);
+    let weight:f32 = input.trim().parse().unwrap();
 
-    own_string(input);
-
-    //let mut mars_weight = calculate_weight_on_mars(100.0);
+    let mut mars_weight = calculate_weight_on_mars(weight);
     
-    //mars_weight = mars_weight * 1000.0;
+    mars_weight = mars_weight * 1000.0;
     
-    //println!("Weight on Mars: {} grams", mars_weight);
+    println!("Your Weight on Planet Mars would be: {} grams", mars_weight);
 }
 
 
 fn calculate_weight_on_mars(weight: f32) -> f32 {
     (weight / 9.81) *3.711
-}
-
-fn borrow_string(s: &String){
-    print!("{}", s);
-}
-
-fn own_string(s: String){
-    print!("{}", s);
 }
