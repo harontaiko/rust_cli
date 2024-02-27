1004683a586b5dabaa975b7166cdf3e16d7d8fae
harontaiko <harontaiko@gmail.com>
2024-02-27 17:43:22 +0300

    small changes

--- entry/src/arrays.rs
+++ entry/src/arrays.rs
@@ -5,6 +5,8 @@ pub fn run() {
 
     let prime_nums: [i32; 9] = [2, 3, 5, 7, 11, 13, 17, 19, 23];
 
+    //let prime_nums2: [i32; 10] = [2,3,5,7,11,13,17,19,23,29];
+
     println!("{:?}", prime_nums);
 
     let mut prime_nums_mut: [i32; 9] = [2, 3, 5, 7, 11, 13, 17, 19, 23];
@@ -18,5 +20,14 @@ pub fn run() {
     //slice
     let slice: &[i32] = &prime_nums_mut[3..4];
 
+    let empty_array: [u32; 0] = [];
+    assert_eq!(&empty_array, &[]);
+
+    for i in 0..prime_nums.len() + 1 {
+        match prime_nums.get(i){
+            some(val) => println!("{}: {}", i, val),
+            None => println!("slow down, array has gone {} too far!", i);
+        }
+    }
     println!("Slices {:?}", slice);
 }
